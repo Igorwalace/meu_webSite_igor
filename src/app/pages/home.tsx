698cffa2269223a0b2d47d2ab37cb2ce1460258d
igoor <igorwallacy3@gmail.com>
2024-06-19 21:00:15 -0300

    erro no navigator

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -17,16 +17,24 @@ const Home = () => {
     const scrollRef = useRef(null);
     const [scrollYPosition, setScrollYPosition] = useState(0);
 
+    const [isAndroid, setIsAndroid] = useState(false);
+
     const linkApp = 'https://expo.dev/artifacts/eas/8YKgMskwTbaBS6L3UppdoV.apk'
-    const IsAndroid = navigator.userAgent.includes('android')
 
     useEffect(() => {
+
+        const userAgent = navigator.userAgent;
+        if (userAgent.includes('android')) {
+            setIsAndroid(true);
+        }
+
         const updateScrollPosition = () => {
             const scrollY = window.scrollY || document.documentElement.scrollTop;
             setScrollYPosition(scrollY);
         };
         window.addEventListener('scroll', updateScrollPosition);
         return () => window.removeEventListener('scroll', updateScrollPosition);
+
     }, [scrollYPosition])
 
     return (
@@ -54,7 +62,7 @@ const Home = () => {
                             <h1 className='md:text-sm text-xs text-[var(--cinza)] max-w-[70%]' >Dou vida à projetos, construindo coisas legais na internet.</h1>
                         </div>
                         <div className='pt-3' >
-                            <h3 className='text-sm max-w-[70%] md:max-w-[70%]' >Você também pode instalar o app deste site clicando<Link className='text-[var(--cinza)]' href={`${IsAndroid ? linkApp : '/'}`} > aqui </Link>(apenas para Android).</h3>
+                            <h3 className='text-sm max-w-[70%] md:max-w-[70%]' >Você também pode instalar o app deste site clicando<Link className='text-[var(--cinza)]' href={`${isAndroid ? linkApp : '/'}`} > aqui </Link>(apenas para Android).</h3>
                         </div>
                     </div>
                     <div className='hidden md:block md:my-32 my-5 md:text-base text-sm text-[var(--cinza)]' >
